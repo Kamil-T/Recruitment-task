04e39f6e0309f627d7a8ed814df39ca83a1b1c77
Kamil-T <trzaskomakamil@gmail.com>
2020-04-18 16:28:55 +0200

    Context provide companies with incomes

--- src/contexts/CompaniesContext.js
+++ src/contexts/CompaniesContext.js
@@ -3,8 +3,6 @@ import React, { createContext, useState, useEffect } from 'react'
 export const CompaniesContext = createContext()
 
 const CompaniesProvider = (props) => {
-  const [companies, setCompanies] = useState(null)
-  const [incomes, setIncomes] = useState(null)
   const [error, setError] = useState(null)
   const [loading, setLoading] = useState(false)
   const [companiesWithIncomes, setCompaniesWithIncomes] = useState(null)
@@ -17,7 +15,24 @@ const CompaniesProvider = (props) => {
           'https://recruitment.hal.skygate.io/companies'
         )
         const jsonComp = await resComp.json()
-        setCompanies(jsonComp)
+
+        let inco = await Promise.all(
+          jsonComp.map(async (company) => {
+            let resInco = await fetch(
+              `https://recruitment.hal.skygate.io/incomes/${company.id}`
+            )
+            let jsonInco = await resInco.json()
+            return jsonInco
+          })
+        )
+
+        const merge = await Promise.all(
+          inco.map((company) => ({
+            ...jsonComp.find((incomes) => incomes.id === company.id),
+            ...company,
+          }))
+        )
+        setCompaniesWithIncomes(merge)
       } catch (e) {
         setError(e)
       } finally {
@@ -27,9 +42,7 @@ const CompaniesProvider = (props) => {
     fetchCompanies()
   }, [])
 
-  console.log(companies)
-  console.log(companiesWithIncomes)
-  const data = [companies, loading, error]
+  const data = [companiesWithIncomes, loading, error]
 
   return (
     <CompaniesContext.Provider value={data}>
