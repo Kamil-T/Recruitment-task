9f534d3d22084a3f4130a3357a95f05ba27e9c34
Kamil-T <trzaskomakamil@gmail.com>
2020-04-21 17:12:31 +0200

    Updates

---
+++ precache-manifest.b598c8c14f06ed66c14f707bd45cc456.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bf44869d7f95209b193c7abb1fa7461",
+    "url": "/recruitment-task/index.html"
+  },
+  {
+    "revision": "9e7f7799326d09d5fe1a",
+    "url": "/recruitment-task/static/css/main.cfb83f22.chunk.css"
+  },
+  {
+    "revision": "0a0d1f477df7e860b389",
+    "url": "/recruitment-task/static/js/2.c1854d2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recruitment-task/static/js/2.c1854d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e7f7799326d09d5fe1a",
+    "url": "/recruitment-task/static/js/main.0b4e19fd.chunk.js"
+  },
+  {
+    "revision": "ef4a75530bd928a9bd72",
+    "url": "/recruitment-task/static/js/runtime-main.41ab3202.js"
+  }
+]);
